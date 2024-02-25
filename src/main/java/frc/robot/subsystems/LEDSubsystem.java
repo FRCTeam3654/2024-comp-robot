a41958661a0c2d7af99e0adaa243d45e87394876
zjwu46 <zjwu46@gmail.com>
2024-02-25 13:02:41 -0500

    Update LEDSubsystem.java

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -11,7 +11,8 @@ public class LEDSubsystem extends SubsystemBase {
   //private final AddressableLED m_led_right = new AddressableLED(RobotMap.LED_PWMPORT_RIGHT);
   private final AddressableLEDBuffer m_led_left_Buffer = new AddressableLEDBuffer(RobotMap.LED_LENGTH);
   private final AddressableLEDBuffer m_led_right_Buffer = new AddressableLEDBuffer(RobotMap.LED_LENGTH);
-  private int m_rainbowFirstPixelHue;
+  private int m_rainbowFirstPixelHue_left;
+  private int m_rainbowFirstPixelHue_right;
 
   public LEDSubsystem() {
     m_led_left.setLength(m_led_left_Buffer.getLength());
@@ -31,7 +32,7 @@ public class LEDSubsystem extends SubsystemBase {
     for (var i = 0; i < m_led_left_Buffer.getLength(); i++) {
       // Calculate the hue - hue is easier for rainbows because the color
       // shape is a circle so only one value needs to precess
-      final var hue = (m_rainbowFirstPixelHue + (i * 180 / m_led_left_Buffer.getLength())) % 180;
+      final var hue = (m_rainbowFirstPixelHue_left + (i * 180 / m_led_left_Buffer.getLength())) % 180;
       // Set the value
       m_led_left_Buffer.setHSV(i, hue, 255, 128);
     }
@@ -39,15 +40,17 @@ public class LEDSubsystem extends SubsystemBase {
      for (var i = 0; i < m_led_right_Buffer.getLength(); i++) {
       // Calculate the hue - hue is easier for rainbows because the color
       // shape is a circle so only one value needs to precess
-      final var hue = (m_rainbowFirstPixelHue + (i * 180 / m_led_right_Buffer.getLength())) % 180;
+      final var hue = (m_rainbowFirstPixelHue_right + (i * 180 / m_led_right_Buffer.getLength())) % 180;
       // Set the value
       m_led_right_Buffer.setHSV(i, hue, 255, 128);
     }
     */
     // Increase by to make the rainbow "move"
-    m_rainbowFirstPixelHue += 3;
+    m_rainbowFirstPixelHue_left += 3;
+    m_rainbowFirstPixelHue_right += 3;
     // Check bounds
-    m_rainbowFirstPixelHue %= 180;
+    m_rainbowFirstPixelHue_left %= 180;
+    m_rainbowFirstPixelHue_right %= 180;
     m_led_left.setData(m_led_left_Buffer);
     //m_led_right.setData(m_led_right_Buffer);
   }
