31695cf0d31688e09b71fecf99fa94aebdbce15d
zjwu46 <zjwu46@gmail.com>
2024-03-01 15:40:31 -0500

    different ways to do wrist motion

--- src/main/java/frc/robot/commands/WristSmartMotion2.java
+++ src/main/java/frc/robot/commands/WristSmartMotion2.java
@@ -9,7 +9,7 @@ import frc.robot.RobotContainer;
 import frc.robot.RobotMap;
 import edu.wpi.first.wpilibj.Timer;
 
-public class WristSmartMotion extends Command {
+public class WristSmartMotion2 extends Command {
   /** Creates a new WristCommand. */
   public static int wristMoveNumber = 0;
   double wristTimer;
@@ -21,12 +21,12 @@ public class WristSmartMotion extends Command {
   private int mode = 0;
   private double targetPos;
 
-  public WristSmartMotion() {
+  public WristSmartMotion2() {
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(RobotContainer.wrist);
   }
 
-  public WristSmartMotion(int mode){
+  public WristSmartMotion2(int mode){
     addRequirements(RobotContainer.wrist);
     this.mode = mode;
   }
@@ -39,84 +39,46 @@ public class WristSmartMotion extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-
-   //if(RobotContainer.oi.intakeDownButton.getAsBoolean() || RobotContainer.oi.intakeUpButton.getAsBoolean() || mode >= 0){
-   if(RobotContainer.oi.intakeDownButton.getAsBoolean() || RobotContainer.oi.intakeUpButton.getAsBoolean() || mode == 1){
-    isSmartMotionButtonPressed = true;
-   }
-
-    if(isSmartMotionButtonPressed == true){
-      wristMoveNumber = wristMoveNumber + 1;
-
-      if(mode == 0){ //moves full down for intake
-        targetPos = -35;
-        wristTimer = Timer.getFPGATimestamp();
-        RobotContainer.wrist.goToPositionBySmartMotion(targetPos);
-
-        System.out.println("should i be moving down");
-        isSmartMotionInProgress = true;
-        //IntakeCommand.changeMode(0);
-      }
-
-      else if(mode == 1){ //moves to storage position
-        wristTimer = Timer.getFPGATimestamp();
-        targetPos = 0;
-
-        RobotContainer.wrist.goToPositionBySmartMotion(targetPos); //this should be the store pos bc the motors zero at start
-        System.out.println("should i be moving to up");
-        isSmartMotionInProgress = true;
-        //IntakeCommand.changeMode(2);
-
-      }
-      
-      else if(mode == 2){ //moves for amp
-        wristTimer = Timer.getFPGATimestamp();
-        targetPos = 20;
-       
-        //RobotContainer.wrist.goToPositionBySmartMotion(targetPos); //change value depending on how much we want it to move
-        System.out.println("should i be moving to amp");
-        isSmartMotionInProgress = true;
-      }
-
-      else if(mode == 3){ //moves for trap
-        wristTimer = Timer.getFPGATimestamp();
-        targetPos = 0.2 * RobotMap.wristFullUpDistance;
-        
-        //RobotContainer.wrist.goToPositionBySmartMotion(targetPos); //change value depending on how much we want it to move
-        System.out.println("should i be moving to trap");
-        isSmartMotionInProgress = true;
-      }
-
-      /* 
-      else if(mode == 4){
-        wristTimer = Timer.getFPGATimestamp();
-    
-        RobotContainer.wrist.goToPositionBySmartMotion(0); //should go to store position
-        System.out.println("should i be moving to trap");
-        isSmartMotionInProgress = true;
-      }
-      */
-
-      else{
-        //RobotContainer.wrist.goToPositionBySmartMotion(currentPos);
-        wristTimer = Timer.getFPGATimestamp();
-      }
-    }
-
+    wristTimer = Timer.getFPGATimestamp();  
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+
+    if(mode == 0){ //moves full down for intake
+        targetPos = -35;
+        RobotContainer.wrist.goToPositionBySmartMotion(targetPos);
+        System.out.println("Wrist moves full down");
+        isSmartMotionInProgress = true;
+      }
+      else if(mode == 1){ //moves to storage position
+        targetPos = 0;
+        RobotContainer.wrist.goToPositionBySmartMotion(targetPos); //this should be the store pos bc the motors zero at start
+        System.out.println("should i be moving to up");
+        isSmartMotionInProgress = true;
+      }
+      else if(mode == 2){ //moves for amp
+        targetPos = -9.5;
+        RobotContainer.wrist.goToPositionBySmartMotion(targetPos); //change value depending on how much we want it to move
+        System.out.println("should i be moving to amp");
+        isSmartMotionInProgress = true;
+      }
+      else if(mode == 3){ //moves for trap
+        //targetPos = 0.2 * RobotMap.wristFullUpDistance;
+        
+        //RobotContainer.wrist.goToPositionBySmartMotion(targetPos); //change value depending on how much we want it to move
+        //System.out.println("should i be moving to trap");
+        //isSmartMotionInProgress = true;
+      }
+
+  }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     isSmartMotionInProgress = false;
-    isSmartMotionButtonPressed = false;
-    //isDownPOVPressed = false;
-    //isUpPOVPressed = false;
-    //mode = 0;
+    mode = 1;
   }
 
   // Returns true when the command should end.
