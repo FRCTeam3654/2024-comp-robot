43464a439a1d280427450d7ed038ee63be76f867
zjwu46 <zjwu46@gmail.com>
2024-03-26 23:33:05 -0400

    Drive to Speaker AprilTag via vision and turn the robot to 0 degree in gyro

--- src/main/java/frc/robot/commands/AutoSpeakerShooterCommand.java
+++ src/main/java/frc/robot/commands/AutoSpeakerShooterCommand.java
@@ -41,7 +41,7 @@ public class AutoSpeakerShooterCommand extends Command {
   public void execute() {
     //RobotContainer.speakerShooter.shootSpeaker(shooterVelocity); // at about 95 (about 20k old value)
     //RobotContainer.speakerShooter.percentOutput(-0.9);
-    RobotContainer.speakerShooter.shootSpeaker(-90);
+    RobotContainer.speakerShooter.shootSpeaker(-95); // make it same as SpeakerShooterCommand
     System.out.println("shoot command");
     if(RobotContainer.speakerShooter.isAtSpeed(-90)){
       //RobotContainer.intakeWheels.intakeSpin(-0.2);
---
+++ src/main/java/frc/robot/commands/TurnToAbsoluteAngleCommand.java
@@ -0,0 +1,73 @@
+package frc.robot.commands;
+
+import frc.robot.Constants;
+import frc.robot.subsystems.SwerveSubsystem;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+
+
+public class TurnToAbsoluteAngleCommand extends Command {
+
+    private final SwerveSubsystem m_robotDrive;
+    private boolean complete = false;
+    private double angle;
+    private double gyro_angle;
+    private double turnTimer ;
+    private double turnTimeout = 2;
+
+    public TurnToAbsoluteAngleCommand(SwerveSubsystem subsystem, double degrees, double timeoutS){
+        m_robotDrive = subsystem;
+        angle = degrees;
+        turnTimeout = timeoutS;
+        addRequirements(subsystem);
+    }
+
+    @Override
+    public void initialize(){
+        turnTimer = Timer.getFPGATimestamp();
+        complete = false;
+        //gyro_angle = angle+m_robotDrive.getYaw().getDegrees(); // need start from current angle + degrees
+        gyro_angle = angle;
+    
+    }
+    
+    @Override
+    public void execute(){
+        
+        double gyroAngle = Math.IEEEremainder( m_robotDrive.getYaw().getDegrees(), 360);
+
+        final double kP = 0.1; // 0.028;
+        SmartDashboard.putNumber("gyroAngle", gyroAngle);
+       
+
+        double err = gyro_angle - gyroAngle;
+        //double speed = MathUtil.clamp(err * kP, -Constants.Swerve.maxAngularVelocity*0.32, Constants.Swerve.maxAngularVelocity*0.32);
+        double speed = MathUtil.clamp(err * kP, -Constants.Swerve.maxAngularVelocity*0.5, Constants.Swerve.maxAngularVelocity*0.5);
+    
+        if (Math.abs(err) > 2 && ( (turnTimer + turnTimeout) > Timer.getFPGATimestamp() )  )  {
+            m_robotDrive.drive(new Translation2d(0,0), speed, false, true);
+        } else {
+            complete = true;
+            System.out.println("turn completed with err = "+err);
+        } 
+    } 
+
+    @Override
+    public void end(boolean inturrupted){
+        m_robotDrive.drive(new Translation2d(0,0), 0, false, true);
+    }
+
+    @Override
+    public boolean isFinished(){
+        if( (turnTimer + turnTimeout) < Timer.getFPGATimestamp() ) {
+            // after 2 second, stop command
+            System.out.println("turn timed out");
+            return true;
+        }
+        return complete;
+    }
+}
